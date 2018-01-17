a9bbd9f4059c0f541d4cb279363598ce1c55af25
Sanyue2333 <562441461@qq.com>
2018-01-18 03:05:47 +0800

    7

--- demo.sh
+++ demo.sh
@@ -5,13 +5,13 @@ else
   mkdir $1
   cd $1
   touch index.html 
-  echo "<!DOCTYPE><title>Hello</title><h1>Hi</h1>" > index.html
+icho "<!DOCTYPE><br><title>Hello</title><br><h1>Hi</h1>" > index.html
   mkdir css js
   cd ./css
   touch style.css
   echo "h1{color: red;}" > style.css
   cd ../js
   touch main.js
-  echo "var string = "Hello World" \n alert(string)" > main.js
+  echo "var string = "Hello World" <br> alert(string)" > main.js
   exit
 fi
