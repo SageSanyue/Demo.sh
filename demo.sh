f0631261d1e5d4af54523e012a2dcc1ef45c1b84
Sanyue2333 <562441461@qq.com>
2018-01-18 02:30:40 +0800

    init

--- demo.sh
+++ demo.sh
@@ -1,11 +1,15 @@
 if [ -d $1 ]; then
-  echo 'error: dir exists'
+  echo "error: dir exists"
   exit
 else
   mkdir $1
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo 'success'
+  echo "<!DOCTYPE>\r\n<title>Hello</title>\r\n<h1>Hi</h1>" > index.html
+  cd css
+  echo "h1{color: red;}" > style.css
+  cd js
+  echo "var string = "Hello World" \r\n alert(string)" > main.js
   exit
 fi
