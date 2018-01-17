16d52928e30508cdc450460dc23aab6aaffa8872
Sanyue2333 <562441461@qq.com>
2018-01-18 02:37:43 +0800

    0118

--- demo.sh
+++ demo.sh
@@ -7,9 +7,7 @@ else
   mkdir css js
   touch index.html css/style.css js/main.js
   echo "<!DOCTYPE>\r\n<title>Hello</title>\r\n<h1>Hi</h1>" > index.html
-  cd css
   echo "h1{color: red;}" > style.css
-  cd js
   echo "var string = "Hello World" \r\n alert(string)" > main.js
   exit
 fi
